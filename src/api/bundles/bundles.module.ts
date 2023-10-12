313a23e7a3885305d6b41f69d57fa5558c8d68a8
gonzalosanto <sancruzgon@gmail.com>
2023-10-12 18:01:16 -0300

    Moved files to upper folder

--- src/api/bundles/bundles.module.ts
+++ src/api/bundles/bundles.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { BundlesController } from './bundles.controller';
 import { BundlesService } from './bundles.service';
 import { bundlesProviders } from './bundles.providers';
-import { DatabaseModule } from 'src/models/database.module';
+import { DatabaseModule } from 'src/core/database.module';
 
 @Module({
   imports: [DatabaseModule],
