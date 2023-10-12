4ba63f56bf24dc5cbdb50c7833a2a54052feddf6
gonzalosanto <sancruzgon@gmail.com>
2023-10-12 18:03:26 -0300

    imports new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
-
+import { BundleModule } from './api/bundles/bundles/bundles.module';
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, BundleModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
