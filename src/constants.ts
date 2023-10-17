4cd1101513e4528fa0e2ccf4d840008cb9e88ee2
gonzalosanto <sancruzgon@gmail.com>
2023-10-17 17:08:48 -0300

    new constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,9 @@
 export const BUNDLE_REPOSITORY = 'bundle_repository';
 export const USER_REPOSITORY = 'user_repository';
+export const Keys = ['bundle', 'name', 'store', 'device_id', 'ua', 'uip']
+export enum DEVICE_CATEGORY {
+    desktop = 1,
+    web_applications = 2,
+    mobile = 3,
+    smart_tv = 4,
+}
