312b970d640656c9fd30fa977d9ec2a0cf2fcd95
gonzalosanto <sancruzgon@gmail.com>
2023-10-17 17:07:22 -0300

    ADD: new file to index services

---
+++ src/api/index.ts
@@ -0,0 +1,10 @@
+import { BundlesService } from './bundles/bundles.service';
+import { NamesService } from './names/names.service';
+import { BundleStoreNamesService } from './bundle-store-names/bundle-store-names.service'
+import { StoreUrlsService } from './store-urls/store-urls.service';
+import { UserAgentsService } from './user-agents/user-agents.service';
+import { DevicesService } from './devices/devices.service';
+import { UipsService } from './uips/uips.service';
+import { StoreNamesService } from './store-names/store-names.service';
+
+export { BundlesService, NamesService, StoreUrlsService, BundleStoreNamesService, StoreNamesService, UserAgentsService, DevicesService, UipsService }
