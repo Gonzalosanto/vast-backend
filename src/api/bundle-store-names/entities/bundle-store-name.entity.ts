1372c261efffc50a84c27af53541d9532395d4e9
gonzalosanto <sancruzgon@gmail.com>
2023-10-18 18:42:26 -0300

    FIX: entities missing id

--- src/api/bundle-store-names/entities/bundle-store-name.entity.ts
+++ src/api/bundle-store-names/entities/bundle-store-name.entity.ts
@@ -4,22 +4,27 @@ import {
   Model,
   ForeignKey,
   BelongsTo,
+  PrimaryKey,
 } from 'sequelize-typescript';
 import { StoreNames } from '../../store-names/entities/store-name.entity';
 import { applicationBundle } from '../../bundles/entities/bundles.entity';
 @Table
 export class BundleStoreName extends Model {
+  @PrimaryKey
+  @Column
+  bsn_id: number
+
   @ForeignKey(() => StoreNames)
   @Column
   storeNameId: number;
 
   @BelongsTo(() => StoreNames)
-  storeName: number;
+  storeName: StoreNames;
 
   @ForeignKey(() => applicationBundle)
   @Column
   applicationBundleId: number;
 
   @BelongsTo(() => applicationBundle)
-  applicationBundle: string;
+  applicationBundle: applicationBundle;
 }
--- src/api/store-names/entities/store-name.entity.ts
+++ src/api/store-names/entities/store-name.entity.ts
@@ -4,6 +4,7 @@ import {
   Model,
   BelongsToMany,
   ForeignKey,
+  PrimaryKey,
 } from 'sequelize-typescript';
 import { applicationStore } from '../../store-urls/entities/store-url.entity';
 import { applicationName } from '../../names/entities/name.entity';
@@ -11,6 +12,10 @@ import { applicationBundle } from '../../bundles/entities/bundles.entity';
 import { BundleStoreName } from '../../bundle-store-names/entities/bundle-store-name.entity';
 @Table
 export class StoreNames extends Model {
+  @PrimaryKey
+  @Column
+  sn_id:number;
+
   @ForeignKey(() => applicationStore)
   @Column
   applicationStoreId: number;
