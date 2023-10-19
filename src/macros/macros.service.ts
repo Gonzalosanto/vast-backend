0aa71d7b5c6d6417363da17f13cd64214e2231e5
gonzalosanto <sancruzgon@gmail.com>
2023-10-19 16:50:24 -0300

    saves bundles data

--- src/macros/macros.service.ts
+++ src/macros/macros.service.ts
@@ -3,7 +3,7 @@ import {BundleStoreNamesService, BundlesService, NamesService, DevicesService, S
 import { OperatingSystemsService } from 'src/api/operating-systems/operating-systems.service';
 import { CreateMacroDto } from './dto/create-macro.dto';
 import { CreateBundleStoreNameDto } from 'src/api/bundle-store-names/dto/create-bundle-store-name.dto';
-import { processFileData, saveRecords } from 'src/utils';
+import { getDeviceRowData, getRowData, processFileData, saveRecords } from 'src/utils';
 
 @Injectable()
 export class MacrosService {
@@ -14,12 +14,14 @@ export class MacrosService {
                 private storeService: StoreUrlsService,
                 private nameService: NamesService,
                 private userAgents: UserAgentsService,
-                private devices: DevicesService){}
+                private devices: DevicesService,
+                private userIps: UipsService){}
 
     async getMacros() {
         return this.bundleStoreNameService.findAll();
     }
 
+    //TODO: Implement creation from form data.
     async createFromFormData(macros: CreateMacroDto): Promise<any>{
         // this.operatingSystemsService.create(macros.os)
         return;
@@ -30,11 +32,23 @@ export class MacrosService {
         await saveRecords(records, (rows: any)=>{
             rows.bundles.map((row: any)=> {this.bundleService.createBundle({ bundle: row })})
             rows.names.map((row: any) => {this.nameService.create({ name: row})})
-            rows.stores.map((row: any) => {this.storeService.create({ store: row})})
+            rows.stores.map((row: any) => {this.storeService.create(row)})
             rows.os.map((os: string) => this.operatingSystemsService.create({os: os}))
-        });
+
+            rows.bundle_list.map((row: any)=>{
+                console.log(row)
+            })
+        }, getRowData);
     }
 
+    async createDevicesFromFileData(fileData: any): Promise<any>{
+        const records = await processFileData(fileData.data);
+        await saveRecords(records, (rows: any)=>{
+            rows.uas.map((row: any)=> {this.userAgents.create({ ua: row })})
+            rows.uips.map((row: any)=> this.userIps.create({uip:row}))
+            rows.deviceids.map((row: any)=> this.devices.create({deviceid: row}))
+        }, getDeviceRowData);
+    }
     updateMacros(macros: any, options: any){
         return;
     }
