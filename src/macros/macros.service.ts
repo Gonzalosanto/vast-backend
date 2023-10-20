c44415a28b57f4e6a6d377816e347bfec1b3b8e9
gonzalosanto <sancruzgon@gmail.com>
2023-10-19 21:18:24 -0300

    WIP: adds references from store to OS

--- src/macros/macros.service.ts
+++ src/macros/macros.service.ts
@@ -1,8 +1,6 @@
 import {Injectable} from '@nestjs/common'
 import {BundleStoreNamesService, BundlesService, NamesService, DevicesService, StoreNamesService, StoreUrlsService, UipsService, UserAgentsService} from '../api/index';
 import { OperatingSystemsService } from 'src/api/operating-systems/operating-systems.service';
-import { CreateMacroDto } from './dto/create-macro.dto';
-import { CreateBundleStoreNameDto } from 'src/api/bundle-store-names/dto/create-bundle-store-name.dto';
 import { getDeviceRowData, getRowData, processFileData, saveRecords } from 'src/utils';
 
 @Injectable()
@@ -22,21 +20,23 @@ export class MacrosService {
     }
 
     //TODO: Implement creation from form data.
-    async createFromFormData(macros: CreateMacroDto): Promise<any>{
+    async createFromFormData(macros: any): Promise<any>{
         // this.operatingSystemsService.create(macros.os)
         return;
     }
-    //TODO: SET RELATIONSHIPS BTWN TABLES
+    //TODO: SET RELATIONSHIPS BTWN stores, names (storeNames tbl), and bundles, storeNames (bsn)
     async createFromFileData(fileData: any): Promise<any>{
         const records = await processFileData(fileData.data);
+        const osInstances = (await this.operatingSystemsService.findAll()).map(os => {return {id: os.dataValues.id, os: os.dataValues.os}});
         await saveRecords(records, (rows: any)=>{
             rows.bundles.map((row: any)=> {this.bundleService.createBundle({ bundle: row })})
             rows.names.map((row: any) => {this.nameService.create({ name: row})})
             rows.stores.map((row: any) => {this.storeService.create(row)})
             rows.os.map((os: string) => this.operatingSystemsService.create({os: os}))
 
-            rows.bundle_list.map((row: any)=>{
-                console.log(row)
+            rows.bundle_list.map(async (row: any)=>{
+                const osId = osInstances.filter(os => {if(os.os == row.os) return os})[0]
+                this.storeService.update('store',row.store, {'operatingSystemId': osId.id})
             })
         }, getRowData);
     }
