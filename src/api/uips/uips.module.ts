dbee43b4cc713a65aec316bda2dd6a302b292330
gonzalosanto <sancruzgon@gmail.com>
2023-10-23 22:39:24 -0300

    REF: removed unnecesary imports

--- src/api/uips/uips.module.ts
+++ src/api/uips/uips.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UipsService } from './uips.service';
-import { UipsController } from './uips.controller';
 import { Uip } from './entities/uip.entity';
 import { DatabaseModule } from 'src/core/database.module';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [UipsController],
   providers: [UipsService, {provide: 'UIP_REPOSITORY', useValue: Uip}],
   exports: [UipsService]
 })
