225f86d21194e6f8c8280c03ac64779f13977460
gonzalosanto <sancruzgon@gmail.com>
2023-10-23 22:24:55 -0300

    OK: loads data from file

--- src/macros/macros.service.ts
+++ src/macros/macros.service.ts
@@ -1,16 +1,10 @@
 import {Injectable} from '@nestjs/common'
-import {BundleStoreNamesService, BundlesService, NamesService, DevicesService, StoreNamesService, StoreUrlsService, UipsService, UserAgentsService} from '../api/index';
-import { OperatingSystemsService } from 'src/api/operating-systems/operating-systems.service';
+import {BundleStoreNamesService, DevicesService, UipsService, UserAgentsService} from '../api/index';
 import { getDeviceRowData, getRowData, processFileData, saveRecords } from 'src/utils';
 
 @Injectable()
 export class MacrosService {
-    constructor(private bundleStoreNameService: BundleStoreNamesService, 
-                private operatingSystemsService: OperatingSystemsService,
-                private storeNameService: StoreNamesService,
-                private bundleService: BundlesService,
-                private storeService: StoreUrlsService,
-                private nameService: NamesService,
+    constructor(private bundleStoreNameService: BundleStoreNamesService,
                 private userAgents: UserAgentsService,
                 private devices: DevicesService,
                 private userIps: UipsService){}
@@ -24,19 +18,11 @@ export class MacrosService {
         // this.operatingSystemsService.create(macros.os)
         return;
     }
-    //TODO: SET RELATIONSHIPS BTWN stores, names (storeNames tbl), and bundles, storeNames (bsn)
     async createFromFileData(fileData: any): Promise<any>{
         const records = await processFileData(fileData.data);
-        const osInstances = (await this.operatingSystemsService.findAll()).map(os => {return {id: os.dataValues.id, os: os.dataValues.os}});
         await saveRecords(records, (rows: any)=>{
-            rows.bundles.map((row: any)=> {this.bundleService.createBundle({ bundle: row })})
-            rows.names.map((row: any) => {this.nameService.create({ name: row})})
-            rows.stores.map((row: any) => {this.storeService.create(row)})
-            rows.os.map((os: string) => this.operatingSystemsService.create({os: os}))
-
-            rows.bundle_list.map(async (row: any)=>{
-                const osId = osInstances.filter(os => {if(os.os == row.os) return os})[0]
-                this.storeService.update('store',row.store, {'operatingSystemId': osId.id})
+            rows.bundle_list.map( async(bundle_row: any)=> {
+                await this.bundleStoreNameService.createWithRelationships(bundle_row);
             })
         }, getRowData);
     }
