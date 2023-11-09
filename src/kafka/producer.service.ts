efdc9385f0d9c5a6835981c52ff6bfd4b0602a29
gonzalosanto <sancruzgon@gmail.com>
2023-11-09 11:32:12 -0300

    ADD: kafka producer

---
+++ src/kafka/producer.service.ts
@@ -0,0 +1,53 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Kafka } from 'kafkajs';
+@Injectable()
+export class ProducerService implements OnModuleInit {
+    private instance: ProducerService;
+    private _isConnected = false;
+    private kafkaClient: any = new Kafka({
+        clientId: 'main-client-producer',
+        brokers: [`${process.env.KAFKA_SERVER}:${process.env.KAFKA_PORT}`]
+    })
+    private producer: any = this.kafkaClient.producer();
+    async onModuleInit() {
+    }
+
+    async topicProducer(topic: string, messages: Array<object>) {
+        //Sends to specified topic a messages list...
+        try {
+            await this.start();
+            await this.producer.send({ topic: topic, messages: messages });
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async handleMessagesToSend(messages: Array<object>) {
+        //Validates message format
+        //returns a formatted messages list 
+        return;
+    }
+
+    public getInstance(): ProducerService {
+        if (!this.instance._isConnected) {
+            return new ProducerService()
+        }
+        return this.instance;
+    }
+
+    async start() {
+        try {
+            await this.producer.connect()
+            this._isConnected = true;
+        } catch (error) {
+            console.error('Error connecting producer to broker: ', error);
+        }
+    }
+
+    async shutdown() {
+        await this.producer.disconnect()
+        this._isConnected = false;
+    }
+
+
+}
