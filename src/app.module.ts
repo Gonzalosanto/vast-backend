7644a83817d3596f53900ff5f889fabe2fe2df54
gonzalosanto <sancruzgon@gmail.com>
2023-11-10 15:55:40 -0300

    removed bundles controller and imports refs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
-import { BundleModule } from './api/bundles/bundles.module';
-import { MacrosModule } from './macros/macros.module';
+import { UsersService } from './api/users/users.service';
+import { UsersController } from './api/users/users.controller';
+import { UsersModule } from './api/users/users.module';
+import { MacrosModule } from './api/macros/macros.module';
 import { ReportsService } from './api/reports/reports.service';
 import { ReportsModule } from './api/reports/reports.module';
 import { ReportsController} from './api/reports/reports.controller';
-import { KafkaModule } from './kafka/kafka.module';
+import { KafkaModule } from './main/kafka/kafka.module';
 
 @Module({
   imports: [
     AuthModule, 
     UsersModule,
-    BundleModule,
     MacrosModule,
     ReportsModule,
     KafkaModule
