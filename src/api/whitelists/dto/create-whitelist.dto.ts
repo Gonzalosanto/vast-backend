6444bee22c8e60c7c6186cff6dda72abb2a5576b
gonzalosanto <sancruzgon@gmail.com>
2023-12-04 18:31:45 -0300

    OK: wl create dto

--- src/api/whitelists/dto/create-whitelist.dto.ts
+++ src/api/whitelists/dto/create-whitelist.dto.ts
@@ -1,5 +1,36 @@
+import { Type } from 'class-transformer';
+import {IsDataURI, IsNotEmpty, IsNumberString, IsPositive, IsString, IsUrl, Length, ValidateNested} from 'class-validator';
 export class CreateWhitelistDto {
+    @IsNotEmpty()
+    @IsNumberString()
+    @IsPositive()
+    @Length(4,6)
+    supply_aid: string;
 
+    @IsNotEmpty()
+    @IsNumberString()
+    @IsPositive()
+    bsn_id: string;
 
-    
+    @ValidateNested({each: true})
+    @Type(()=> Bundle)
+    bundleList: Bundle[];
+}
+
+class Bundle{
+    @IsNotEmpty()
+    @IsString()
+    @IsDataURI()
+    bundle: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsDataURI()
+    @IsUrl()
+    store: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsDataURI()
+    name: string;
 }
