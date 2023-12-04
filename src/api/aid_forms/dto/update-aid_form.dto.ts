544d4855e1cc9e7a0e17410ed00f808a47217eae
gonzalosanto <sancruzgon@gmail.com>
2023-12-04 18:30:00 -0300

    DELETE: aid_form changed to wl_metadata

--- src/api/aid_forms/dto/update-aid_form.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAidFormDto } from './create-aid_form.dto';
-
-export class UpdateAidFormDto extends PartialType(CreateAidFormDto) {}
