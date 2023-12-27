6c47302f4458280343426afb1c9225b8296dd985
gonzalosanto <sancruzgon@gmail.com>
2023-12-26 22:59:05 -0300

    FIX: imports module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,7 @@ import { UsersService } from './api/users/users.service';
 import { UsersController } from './api/users/users.controller';
 import { UsersModule } from './api/users/users.module';
 import { MacrosModule } from './api/macros/macros.module';
-import { ReportsService } from './api/reports/reports.service';
 import { ReportsModule } from './api/reports/reports.module';
-import { ReportsController} from './api/reports/reports.controller';
 import { WhitelistsModule } from './api/whitelists/whitelists.module';
 import { KafkaModule } from './main/kafka/kafka.module';
 import { UrlProducerService } from './main/url-producer/url-producer.service';
@@ -20,9 +18,10 @@ import { UserAgentsModule } from './main/user-agents/user-agents.module';
     MacrosModule,
     ReportsModule,
     WhitelistsModule,
+    ReportsModule,
     KafkaModule
   ],
-  controllers: [UsersController, ReportsController],
-  providers: [UsersService, ReportsService, UrlProducerService],
+  controllers: [UsersController],
+  providers: [UsersService,  UrlProducerService],
 })
 export class AppModule {}
